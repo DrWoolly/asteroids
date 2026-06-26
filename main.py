5949ff6661784da59f9594534103c65425b4965f
woolly <drwoolly69@gmail.com>
2026-06-26 16:37:38 +0100

    update main & .gitignore

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt: float = 0.0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
@@ -20,6 +22,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
